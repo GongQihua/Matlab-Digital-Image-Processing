b21b26f99eb04dd2ac0ebffa7ae5326af3338b77
QIHUA GONG <44483974+GongQihua@users.noreply.github.com>
2020-08-29 20:12:21 -0500

    Delete HW1mentor1.m

--- code/HW1mentor1.m
+++
@@ -1,9 +0,0 @@
-im1 = imread('D:/data/6.jpg');
-s = size(im1);
-im2 = rgb2gray(im1);
-s_gray = size(im2);
-figure;
-imshow(im2);
-im3 = imadjust(im2,[50/255 150/255],[0 1],[]); 
-figure;
-imshow(im3);
